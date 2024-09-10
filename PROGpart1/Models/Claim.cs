19f3e771bd073190ef6c17f8f963640deb0821e6
kheni <kheni@VAMBE>
2024-09-10 16:06:45 +0200

    Add project files.

---
+++ PROGpart1/Models/Claim.cs
@@ -0,0 +1,12 @@
+﻿namespace PROGpart1.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+        public string LecturerName { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public string Status { get; set; } // e.g., "Pending", "Approved", "Rejected"
+        public string SupportDocuments { get; set; } // Path to uploaded documents
+        public string Details { get; set; }
+    }
+}
